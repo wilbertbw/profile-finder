8fe4f954a7f1c6f4dee9f9e99d231b1bd88494dd
wilbertbw <wilbertbwibowo@gmail.com>
2025-05-20 16:12:12 -0400

    added a json format .txt file with a json to html converter

---
+++ json-to-html.py
@@ -0,0 +1,18 @@
+import json
+
+html = "<html><head><title>Output</title></head><body>"
+
+with open("example-json-output.txt", "r") as json_file, open("output.html", "w") as output_file:
+  json_data = json_file.read()
+  json_data_dict = json.loads(json_data)
+
+  # modify html string here
+  for profile in json_data_dict:
+    html = html + "<h3>" + profile["name"] + "</h3>"
+    html = html + "<p>Experience: " + profile["experience"] + "</p>"
+    html = html + "<p>Education: " + profile["education"] + "</p>"
+    html = html + "<p>Major: " + profile["major"] + "</p>"
+
+  html += "</body></html>"
+
+  output_file.write(html)
